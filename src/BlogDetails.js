75bffc31b4701aa1834088d21433123637ae6ed3
Rajan-226 <rajan0184.cse19@chitkara.edu.in>
2021-08-10 18:25:56 +0530

    editFeature-partiallyCompleted

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,30 +1,70 @@
 import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
+import { useState } from "react";
+import TextEditor from "./TextEditor";
+//check use params
+
+// TODO: fetch
 
 const BlogDetails = () => {
-    
+
+    const [editingMode, setEditingMode] = useState(false);
+    const [NewBody, setNewBody] = useState(null);
+
     const { id } = useParams();
-    const { data : blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id);
+    const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
     const history = useHistory();
-    
-    const handleClick = () => {
+
+
+    const handleDelete = () => {
         fetch('http://localhost:8000/blogs/' + blog.id, {
-            method : 'DELETE'
+            method: 'DELETE'
         }).then(() => {
             history.push('/');
         });
     }
-    
+
+
+    const handleEdit = () => {
+        setEditingMode(true);
+    }
+
+    const handleSubmit = () => {
+        const axios = require('axios');
+
+        axios.put('http://localhost:8000/blogs/' + blog.id, {
+            author: blog.author,
+            title: blog.title,
+            body: NewBody
+        }).then(resp => {
+            setEditingMode(false);
+        }).catch(error => {
+            console.log(error);
+        });
+
+    }
+
     return (
         <div className="blog-details">
-            { isPending && <div>Loading...</div> }
-            { error && <div>{ error }</div> }
-            { blog && (
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
                 <article>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by <span>{blog.author}</span></p>
-                    <div dangerouslySetInnerHTML={{__html : blog.body}} />
-                    <button onClick={handleClick}>delete</button>
+                    <h2>{blog.title}</h2>
+                    <p>Written by <span className="authorName">{blog.author}</span></p>
+                    <br />
+
+                    {!editingMode && <div dangerouslySetInnerHTML={{ __html: blog.body }} />}
+                    {editingMode && <TextEditor setBody={setNewBody} lastValue={blog.body} />}
+
+                    <br />
+
+                    {!editingMode && <button onClick={handleEdit}>Edit</button>}
+                    {editingMode && <button onClick={handleSubmit}>Submit</button>}
+
+                    {' '}
+
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>
