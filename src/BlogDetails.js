b3dff2e9c384ff9114c1a98a90a8064c30419661
Rajan-226 <rajan0184.cse19@chitkara.edu.in>
2021-08-24 18:18:01 +0530

    added_like/dislike/comment_to_blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,22 +1,21 @@
 import { useHistory, useParams } from "react-router";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import TextEditor from "./TextEditor";
 import firebase from "./firebase";
 import { useAuth } from './provider/AuthContext';
+import { ThumbsUpIcon, ThumbsDownIcon, toaster, Pane, SendMessageIcon, TextInputField } from 'evergreen-ui'
 
 const BlogDetails = () => {
 
     const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
     const [editingMode, setEditingMode] = useState(false);
     const [NewBody, setNewBody] = useState(null);
     const [blog, setBlog] = useState(null);
     const { currentUser } = useAuth();
-
     const { id } = useParams(); //picks the id from the link
     const database = firebase.database().ref('blogs/' + id);
-
     const history = useHistory();
+    const commentRef = useRef('');
 
     useEffect(() => {
 
@@ -47,31 +46,171 @@ const BlogDetails = () => {
         setEditingMode(false);
     }
 
+    function addComment(e) {
+        e.preventDefault();
+        if (commentRef.current.value.length === 0) {
+            toaster.warning('Comment cannot be empty', { id: 'forbidden-action' });
+            return;
+        }
+        if (!currentUser) {
+            toaster.warning('Please login first to comment on this blog', { id: 'forbidden-action' });
+            e.target.reset();
+            return;
+        }
+
+        let NewComments = blog.comments;
+        NewComments.push([commentRef.current.value, currentUser.displayName]);
+
+        database.update({
+            comments: NewComments
+        });
+
+        e.target.reset();
+    }
+
+    function handleOpinion(opinion) {
+        let likes = blog.likes;
+        let dislikes = blog.dislikes;
+        if (likes.indexOf(currentUser.uid) != -1 || dislikes.indexOf(currentUser.uid) != -1) {
+            toaster.warning('You cannot vote twice', { id: 'forbidden-action' });
+            return;
+        }
+        if (opinion === 1) {
+            likes.push(currentUser.uid);
+            database.update({
+                likes: likes
+            });
+        } else {
+            dislikes.push(currentUser.uid);
+            database.update({
+                dislikes: dislikes
+            });
+        }
+    }
+
     return (
         <div className="blog-details">
             {isPending && <div>Loading...</div>}
-            {error && <div>{error}</div>}
             {blog && (
-                <article>
-                    <h2>{blog.title}</h2>
-                    <p>Written by <span className="authorName">{blog.author}</span></p>
+                <>
+                    <article>
+                        <h2>{blog.title}</h2>
+                        <p>Written by <span className="authorName">{blog.author}</span></p><br />
+                        {editingMode && <TextEditor setBody={setNewBody} lastValue={blog.body} />}
+                        {!editingMode && <div dangerouslySetInnerHTML={{ __html: blog.body }} />}
+                    </article>
+
                     <br />
+                    {
+                        !currentUser &&
+                        <>
+                            <ThumbsUpIcon cursor="pointer" onClick={() => toaster.warning('Please log in first to like this blog', { id: 'forbidden-action' })} size={40} />
+                            &emsp;{blog.likes && blog.likes.length - 1}
+                            &emsp;&emsp;
+                            <ThumbsDownIcon cursor="pointer" onClick={() => toaster.warning('Please log in first to unlike this blog', { id: 'forbidden-action' })} size={40} />
+                            &emsp;{blog.dislikes.length - 1}
 
-                    {!editingMode && <div dangerouslySetInnerHTML={{ __html: blog.body }} />}
-                    {editingMode && <TextEditor setBody={setNewBody} lastValue={blog.body} />}
+                            <br />
+                            <br />
+                            <br />
+                            <h1>Comments</h1>
+                            {
+                                blog.comments && blog.comments.map((comment, i) => {
+                                    if (i)
+                                        return (
+                                            <Pane elevation={3} height="50px" marginTop="10px" display="flex" alignItems="center" flexDirection="column" justifyContent="center" border="default">
+                                                {comment[0] + " written by " + comment[1]}
+                                            </Pane>
+                                        )
+                                })
+                            }
+                            <form onSubmit={addComment} >
+                                <Pane display="flex" justifyContent="center" alignItems="center" border="2px solid black" marginTop="30px">
+                                    <TextInputField ref={commentRef} width="80%" height="1" placeholder="Enter your comment" />
+                                    &emsp;
+                                    <button type="submit"><SendMessageIcon cursor="pointer" size={35} /></button>
+                                </Pane>
+                            </form>
 
-                    <br />
+                        </>
+                    }
+                    {
+                        currentUser && currentUser.uid === blog.id &&
+                        <>
+                            {!editingMode && <button id="delEdit" onClick={handleEdit}>Edit</button>}
+                            {editingMode && <button id="delEdit" onClick={handleSubmit}>Submit</button>}
+                            &emsp;
+                            <button id="delEdit" onClick={handleDelete}>Delete</button>
+                            <br /><br /><br />
 
-                    {currentUser && currentUser.uid === blog.id && !editingMode && <button onClick={handleEdit}>Edit</button>}
-                    {currentUser && currentUser.uid === blog.id && editingMode && <button onClick={handleSubmit}>Submit</button>}
+                            <ThumbsUpIcon cursor="pointer" onClick={() => toaster.warning('You cannot like your own blog', { id: 'forbidden-action' })} size={40} />
+                            &emsp;{blog.likes && blog.likes.length - 1}
+                            &emsp;&emsp;
+                            <ThumbsDownIcon cursor="pointer" onClick={() => toaster.warning('You cannot unlike your own blog', { id: 'forbidden-action' })} size={40} />
+                            &emsp;{blog.dislikes.length - 1}
 
-                    {' '}
+                            <br />
+                            <br />
+                            <br />
+                            <h1>Comments</h1>
 
-                    {currentUser && currentUser.uid === blog.id && <button onClick={handleDelete}>Delete</button>}
+                            {
+                                blog.comments.map((comment, i) => {
+                                    if (i)
+                                        return (
+                                            <Pane elevation={3} height="50px" marginTop="10px" display="flex" alignItems="center" flexDirection="column" justifyContent="center" border="default">
+                                                {comment[0] + " written by " + comment[1]}
+                                            </Pane>
+                                        )
+                                })
+                            }
 
-                </article>
-            )}
-        </div>
+                            <form onSubmit={addComment} >
+                                <Pane display="flex" justifyContent="center" alignItems="center" border="2px solid black" marginTop="30px">
+                                    <TextInputField ref={commentRef} width="80%" height="1" placeholder="Enter your comment" />
+                                    &emsp;
+                                    <button type="submit"><SendMessageIcon cursor="pointer" size={35} /></button>
+                                </Pane>
+                            </form>
+                        </>
+                    }
+                    {
+                        currentUser && currentUser.uid !== blog.id &&
+                        <>
+                            <ThumbsUpIcon cursor="pointer" onClick={() => handleOpinion(1)} size={40} />
+                            &emsp;{blog.likes && blog.likes.length - 1}
+                            &emsp;&emsp;
+                            <ThumbsDownIcon cursor="pointer" onClick={() => handleOpinion(0)} size={40} />
+
+                            &emsp;{blog.dislikes.length - 1}
+
+                            <br />
+                            <br />
+                            <br />
+                            <h1>Comments</h1>
+                            {
+                                blog.comments.map((comment, i) => {
+                                    if (i)
+                                        return (
+                                            <Pane elevation={3} height="50px" marginTop="10px" display="flex" alignItems="center" flexDirection="column" justifyContent="center" border="default">
+                                                {comment[0] + " written by " + comment[1]}
+                                            </Pane>
+                                        )
+                                })
+                            }
+                            <form onSubmit={addComment} >
+                                <Pane display="flex" justifyContent="center" alignItems="center" border="2px solid black" marginTop="30px">
+                                    <TextInputField ref={commentRef} width="80%" height="1" placeholder="Enter your comment" />
+                                    &emsp;
+                                    <button type="submit"><SendMessageIcon cursor="pointer" size={35} /></button>
+                                </Pane>
+                            </form>
+                        </>
+                    }
+                </>
+            )
+            }
+        </div >
     );
 }
 
